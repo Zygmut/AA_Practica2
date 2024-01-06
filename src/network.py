de3a7b36dfcf5081a9cddfb0252db5e2e47314a9
Zygmut <deltas.game@gmail.com>
2024-01-07 00:19:20 +0100

    Whatever will be, will be 8{

--- src/network.py
+++ src/network.py
@@ -1,3 +1,5 @@
+from torchvision import models
+
 from torch.nn import (
     Module,
     Conv2d,
@@ -12,31 +14,19 @@ from torch.nn import (
 
 
 class CatDogNet(Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super(CatDogNet, self).__init__()
-        self.conv_block1 = CatDogNet.convolutional_block(3, 64)
-        self.conv_block2 = CatDogNet.convolutional_block(64, 128)
-        self.conv_block3 = CatDogNet.convolutional_block(128, 256)
-        self.conv_block4 = CatDogNet.convolutional_block(256, 512)
-
-        self.relu = ReLU()
-        self.dropout = Dropout(0.5)
-        self.flatten = Flatten()
-
-        self.linear1 = Linear(512 * 8 * 8, 1024)
-        self.linear2 = Linear(1024, 2)
-
         self.layers = Sequential(
-            self.conv_block1,
-            self.conv_block2,
-            self.conv_block3,
-            self.conv_block4,
-            self.dropout,
-            self.flatten,
-            self.linear1,
-            self.relu,
-            self.dropout,
-            self.linear2,
+            CatDogNet.convolutional_block(3, 64),
+            CatDogNet.convolutional_block(64, 128),
+            CatDogNet.convolutional_block(128, 256),
+            CatDogNet.convolutional_block(256, 512),
+            Flatten(),
+            Linear(512 * 8 * 8, 1024),
+            ReLU(),
+            Dropout(0.5),
+            # By default this will be binary
+            Linear(1024, num_classes),
         )
 
     def forward(self, x):
@@ -58,3 +48,52 @@ class CatDogNet(Module):
             ReLU(),
             MaxPool2d(kernel_size=2, stride=2),
         )
+
+
+class CatDogResnet50(Module):
+    def __init__(self, num_classes):
+        super(CatDogResnet50, self).__init__()
+
+        base_resnet50_model = models.resnet50(
+            weights=models.resnet.ResNet50_Weights.DEFAULT
+        )
+
+        # Remove the final layer to replace it with a binary classificatior
+        base_model_resnet50 = Sequential(*list(base_resnet50_model.children())[:-1])
+
+        self.layers = Sequential(
+            base_model_resnet50,
+            Flatten(),
+            Linear(512 * 4, 512),
+            ReLU(),
+            Dropout(0.5),
+            Linear(512, num_classes),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class CatDogResnet34(Module):
+    def __init__(self, num_classes):
+        super(CatDogResnet34, self).__init__()
+
+        base_resnet_model = models.resnet34(
+            weights=models.resnet.ResNet34_Weights.DEFAULT
+        )
+
+        # Remove the final layer to replace it with the
+        # 37 target outputs
+        base_model = Sequential(*list(base_resnet_model.children())[:-1])
+
+        self.layers = Sequential(
+            base_model,
+            Flatten(),
+            Linear(512, 512),
+            ReLU(),
+            Dropout(0.5),
+            Linear(512, num_classes),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
